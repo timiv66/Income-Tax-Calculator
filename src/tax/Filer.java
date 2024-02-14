4f6cf1f3096011f11357c1993391866e1a1ef762
Timi Verissimo <timiv9@gmail.com>
2024-02-13 19:34:17 -0500

    Filer class

--- src/tax/Filer.java
+++ src/tax/Filer.java
@@ -48,139 +48,145 @@ public class Filer {
 	
 	public double calcAGI() {
 		double AGI;
-		AGI = annualIncome-atldeductions;
+		AGI = getAnnualIncome()-getAtldeductions();
 		return AGI;
 	}
 	
 	public double calcTaxIncome() {
 		double taxableInc = 0;
-		if (status.matches("Single") || status.matches("Married File Separately")) {
-			if (itmdeductions == 0) {
+		if (getStatus().matches("Single") || getStatus().matches("Married File Separately")) {
+			if (getItmdeductions() == 0) {
 				taxableInc = calcAGI() - 13850;
 			}else {
-				taxableInc = calcAGI() - itmdeductions;
+				taxableInc = calcAGI() - getItmdeductions();
 			}	
 		}
-		else if (status.matches("Married File Together")) {
-			if(itmdeductions == 0) {
+		else if (getStatus().matches("Married File Together")) {
+			if(getItmdeductions() == 0) {
 				taxableInc=calcAGI()-27700;
 			}
 			else {
-				taxableInc=calcAGI()-itmdeductions;	
+				taxableInc=calcAGI()-getItmdeductions();	
 			}
 		}	
-		else if (status.matches("Head of Household")) {
-			if(itmdeductions == 0) {
+		else if (getStatus().matches("Head of Household")) {
+			if(getItmdeductions() == 0) {
 				taxableInc=calcAGI()-20800;
 			}
 			else {
-				taxableInc=calcAGI()-itmdeductions;
+				taxableInc=calcAGI()-getItmdeductions();
 			}
 		}
 		return taxableInc;
 	}
 	
 	public double calcGrossTax() {
-		double grossTaxLib = 0;
-		switch (status) {
-			case "Signle"://gross tax liability for single
-				if(calcTaxIncome()>0 && calcTaxIncome()<=11000) {
-					grossTaxLib=calcTaxIncome()*.10;
-				}
-				else if(calcTaxIncome()>11000 && calcTaxIncome()<=44725) {
-					grossTaxLib=calcTaxIncome()*.12;
-				}
-				else if(calcTaxIncome()>44725 && calcTaxIncome()<=95375) {
-					grossTaxLib=calcTaxIncome()*.22;
-				}
-				else if(calcTaxIncome()>95375 && calcTaxIncome()<=182100) {
-					grossTaxLib=calcTaxIncome()*.24;
-				}
-				else if(calcTaxIncome()>182100 && calcTaxIncome()<=231250) {
-					grossTaxLib=calcTaxIncome()*.32;
-				}
-				else if(calcTaxIncome()>231250 && calcTaxIncome()<=578125) {
-					grossTaxLib=calcTaxIncome()*.35;
-				}
-				else if(calcTaxIncome()>578125) {
-					grossTaxLib=calcTaxIncome()*.37;		
-				}
-				
-				break;
-			
-			case"Married File Separately"://gross tax liability for Married File Separately
-				if(calcTaxIncome()>0 && calcTaxIncome()<=11000) {
-					grossTaxLib=calcTaxIncome()*.10;
-				}
-				else if(calcTaxIncome()>11000 && calcTaxIncome()<=44725) {
-					grossTaxLib=calcTaxIncome()*.12;
-				}
-				else if(calcTaxIncome()>44725 && calcTaxIncome()<=95375) {
-					grossTaxLib=calcTaxIncome()*.22;
-				}
-				else if(calcTaxIncome()>95375 && calcTaxIncome()<=182100) {
-					grossTaxLib=calcTaxIncome()*.24;
-				}
-				else if(calcTaxIncome()>182100 && calcTaxIncome()<=231250) {
-					grossTaxLib=calcTaxIncome()*.32;
-				}
-				else if(calcTaxIncome()>231250 && calcTaxIncome()<=346875) {
-					grossTaxLib=calcTaxIncome()*.35;
-				}
-				else if(calcTaxIncome()>346875) {
-					grossTaxLib=calcTaxIncome()*.37;
-				}
-				break;
-				
-			case"Married File Together"://gross tax liability for Married File Separately Together
-				if(calcTaxIncome()>0 && calcTaxIncome()<=22000) {
-					grossTaxLib=calcTaxIncome()*.10;
-				}
-				else if(calcTaxIncome()>22000 && calcTaxIncome()<=89450) {
-					grossTaxLib=calcTaxIncome()*.12;
-				}
-				else if(calcTaxIncome()>89450 && calcTaxIncome()<=190750) {
-					grossTaxLib=calcTaxIncome()*.22;
-				}
-				else if(calcTaxIncome()>190750 && calcTaxIncome()<=364200) {
-					grossTaxLib=calcTaxIncome()*.24;
-				}
-				else if(calcTaxIncome()>364200 && calcTaxIncome()<=462500) {
-					grossTaxLib=calcTaxIncome()*.32;
-				}
-				else if(calcTaxIncome()>462500 && calcTaxIncome()<=693750) {
-					grossTaxLib=calcTaxIncome()*.35;
-				}
-				else if(calcTaxIncome()>693750) {
-					grossTaxLib=calcTaxIncome()*.37;
-				}
-				break;
-				
-			case"Head of Household"://gross tax liability for Head of Household
-				if(grossTaxLib>0 && grossTaxLib<=15700) {
-					grossTaxLib=grossTaxLib*.10;
-				}
-				else if(calcTaxIncome()>15700 && calcTaxIncome()<=59850) {
-					grossTaxLib=calcTaxIncome()*.12;
-				}
-				else if(calcTaxIncome()>59850 && calcTaxIncome()<=95350) {
-					grossTaxLib=calcTaxIncome()*.22;
-				}
-				else if(calcTaxIncome()>95350 && calcTaxIncome()<=182100) {
-					grossTaxLib=calcTaxIncome()*.24;
-				}
-				else if(calcTaxIncome()>182100 && calcTaxIncome()<=231250) {
-					grossTaxLib=calcTaxIncome()*.32;
-				}
-				else if(calcTaxIncome()>231250 && calcTaxIncome()<=578100) {
-					grossTaxLib=calcTaxIncome()*.35;
-				}
-				else if(calcTaxIncome()>578100) {
-					grossTaxLib=calcTaxIncome()*.37;
-				}
-				break;	
+		double grossTax = 0;
+		//Single filers
+		if (getStatus().matches("Single")) {
+			if(calcTaxIncome() >= 0 && calcTaxIncome() <= 11000) {
+				grossTax = calcTaxIncome() * .10;
+				return grossTax;
+			}else if(calcTaxIncome() > 11000 && calcTaxIncome() <= 44725) {
+				grossTax = calcTaxIncome() * .12;
+				return grossTax;
+			}else if(calcTaxIncome() > 44725 && calcTaxIncome() <= 95375) {
+				grossTax = calcTaxIncome() * .22;
+				return grossTax;
+			}else if(calcTaxIncome() > 95375 && calcTaxIncome() <= 182100) {
+				grossTax = calcTaxIncome() * .24;
+				return grossTax;
+			}else if(calcTaxIncome() > 182100 && calcTaxIncome() <= 231250) {
+				grossTax = calcTaxIncome() * .32;
+				return grossTax;
+			}else if(calcTaxIncome() > 231250 && calcTaxIncome() <= 578125) {
+				grossTax = calcTaxIncome() * .35;
+				return grossTax;
+			}else if(calcTaxIncome() > 578125) {
+				grossTax = calcTaxIncome() * .37;
+				return grossTax;
+			}
 		}
-		return grossTaxLib;
+		
+		//Married Filed Together
+		else if (getStatus().matches("Married File Together")) {
+			if(calcTaxIncome() >= 0 && calcTaxIncome() <= 22000) {
+				grossTax = calcTaxIncome() * .10;
+				return grossTax;
+			}else if(calcTaxIncome() > 22000 && calcTaxIncome() <= 89450) {
+				grossTax = calcTaxIncome() * .12;
+				return grossTax;
+			}else if(calcTaxIncome() > 89450 && calcTaxIncome() <= 190750) {
+				grossTax = calcTaxIncome() * .22;
+				return grossTax;
+			}else if(calcTaxIncome() > 190750 && calcTaxIncome() <= 364200) {
+				grossTax = calcTaxIncome() * .24;
+				return grossTax;
+			}else if(calcTaxIncome() > 364200 && calcTaxIncome() <= 462500) {
+				grossTax = calcTaxIncome() * .32;
+				return grossTax;
+			}else if(calcTaxIncome() > 462500 && calcTaxIncome() <= 693750) {
+				grossTax = calcTaxIncome() * .35;
+				return grossTax;
+			}else if(calcTaxIncome() > 693751) {
+				grossTax = calcTaxIncome() * .37;
+				return grossTax;
+			}
+		}
+		
+		//Married File Separately
+		else if (getStatus().matches("Married File Separately")) {
+			if(calcTaxIncome() >= 0 && calcTaxIncome() <= 11000) {
+				grossTax = calcTaxIncome() * .10;
+				return grossTax;
+			}else if(calcTaxIncome() > 11000 && calcTaxIncome() <= 44725) {
+				grossTax = calcTaxIncome() * .12;
+				return grossTax;
+			}else if(calcTaxIncome() > 44725 && calcTaxIncome() <= 95375) {
+				grossTax = calcTaxIncome() * .22;
+				return grossTax;
+			}else if(calcTaxIncome() > 95375 && calcTaxIncome() <= 182100) {
+				grossTax = calcTaxIncome() * .24;
+				return grossTax;
+			}else if(calcTaxIncome() > 182100 && calcTaxIncome() <= 231250) {
+				grossTax = calcTaxIncome() * .32;
+				return grossTax;
+			}else if(calcTaxIncome() > 231250 && calcTaxIncome() <= 346875) {
+				grossTax = calcTaxIncome() * .35;
+				return grossTax;
+			}else if(calcTaxIncome() > 346875) {
+				grossTax = calcTaxIncome() * .37;
+				return grossTax;
+			}
+		}
+		
+		//Head of Household
+		else if (getStatus().matches("Head of Household")) {
+			if(calcTaxIncome() >= 0 && calcTaxIncome() <= 15700) {
+				grossTax = calcTaxIncome() * .10;
+				return grossTax;
+			}else if(calcTaxIncome() > 15700 && calcTaxIncome() <= 59850) {
+				grossTax = calcTaxIncome() * .12;
+				return grossTax;
+			}else if(calcTaxIncome() > 59850 && calcTaxIncome() <= 95350) {
+				grossTax = calcTaxIncome() * .22;
+				return grossTax;
+			}else if(calcTaxIncome() > 95350 && calcTaxIncome() <= 182100) {
+				grossTax = calcTaxIncome() * .24;
+				return grossTax;
+			}else if(calcTaxIncome() > 182100 && calcTaxIncome() <= 231250) {
+				grossTax = calcTaxIncome() * .32;
+				return grossTax;
+			}else if(calcTaxIncome() > 231250 && calcTaxIncome() <= 578100) {
+				grossTax = calcTaxIncome() * .35;
+				return grossTax;
+			}else if(calcTaxIncome() > 578100) {
+				grossTax = calcTaxIncome() * .37;
+				return grossTax;
+			}
+		}
+		
+		return grossTax;
 	}
 	
 	public double calctaxDueorRef() {
@@ -206,29 +212,7 @@ public class Filer {
 		filer1.setItmdeductions(0);
 		filer1.setStatus("Single");
 		System.out.println(filer1.calcTaxIncome());
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	
 		
 	}
 	
